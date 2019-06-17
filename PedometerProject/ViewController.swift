ec6713c4f0307eb98efae1099e96cbea772e15cf
Zewu Chen <contact.zewu@gmail.com>
2019-06-17 17:07:04 -0300

    finished function getData

--- PedometerProject/ViewController.swift
+++ PedometerProject/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblPassos: UILabel!
+    @IBOutlet weak var lblDistancia: UILabel!
+    @IBOutlet weak var lblAviso: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
     }
+    
+    let pedometer = CMPedometer()
+    
+    func getData(){
+        let date = Date()
+        let dateAnt = Calendar.current.date(byAdding: .hour, value: -6, to: Date())
+        
+        if let dateAntes = dateAnt{
+            pedometer.queryPedometerData(from: dateAntes, to: date) { (dados, erros) in
+                guard let data = dados, erros == nil else { return }
 
+                DispatchQueue.main.async {
+                    self.lblPassos.text = data.numberOfSteps.stringValue
+                    if let a = data.distance?.stringValue{
+                        var texto = a.split(separator: ".")
+                        self.lblDistancia.text =  String(Double(texto[0].description)! / 1000) + " km"
+                        self.lblAviso.text = "VOCÊ PODIA TER CONTADO \(String(texto[0])) METROS NO PÓKEMON GO"
+                    }
+                }
+                
+            }
+        }
+        
+        
+    }
 
 }
 
